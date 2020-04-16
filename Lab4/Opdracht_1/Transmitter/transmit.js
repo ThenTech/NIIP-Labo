d9a712f2000a6d3d8b28f14de4d6268422915ef1
Bram Kelchtermans <bramkel@gmail.com>
2020-04-16 15:27:02 +0200

    Brightness

--- Lab4/Opdracht_1/Transmitter/transmit.js
+++ Lab4/Opdracht_1/Transmitter/transmit.js
@@ -1,22 +1,20 @@
 var span = 250;
 var prev = 0;
-let input_text  = document.getElementById("text");
-let input_bps   = document.getElementById("bps");
+let input_text = document.getElementById("text");
+let input_bps = document.getElementById("bps");
 let transmitter = document.getElementById("transmit-box");
 let clock = document.getElementById("clock-box");
 let mode_label = document.getElementById("mode-label");
-let clock_enabled = false;
-function toggleClock() {
-    if(!clock_enabled) {
+let settings_block = document.getElementById("settings-block");
+let mode_select = document.getElementById("mode");
+let settings_shown = false;
+function toggleClock(clock_enabled) {
+    if (!clock_enabled) {
         // Show clock div
         clock.style.display = "block";
         // Set height of containers
         transmitter.style.height = "45vh";
         clock.style.height = "45vh";
-        // Change mode label
-        mode_label.innerText = "Cl."
-        // Set boolean
-        clock_enabled = true;
     } else {
         // Show clock div
         clock.style.display = "none";
@@ -24,23 +22,38 @@ function toggleClock() {
         transmitter.style.height = "90vh";
         // Change mode label
         mode_label.innerText = "BPS";
-        // Set boolean
-        clock_enabled = false;
     }
+}
+function changeMode() {
+    let mode = mode_select.value;
+    if (mode.localeCompare("clock")) {
+        toggleClock(true);
+    } else {
+        toggleClock(false);
+    }
+}
+
+function toggleSettings() {
+    var disp = "block";
+    if (settings_shown) {
+        disp = "none"
+    }
+    settings_block.style.display = disp;
+    settings_shown = !settings_shown;
 }
 
 function transmit() {
     let start_symbol = "11110011";
-    let stop_symbol  = stringToBytes('\n')[0];
+    let stop_symbol = stringToBytes('\n')[0];
 
-    const text = input_text.value ;
+    const text = input_text.value;
 
     span = 1000 / +input_bps.value;
 
     console.log("Used span: " + span);
     var bytes = [start_symbol];
     const strBytes = stringToBytes(text);
-    for(let i = 0; i < strBytes.length; i++) {
+    for (let i = 0; i < strBytes.length; i++) {
         bytes.push(strBytes[i]);
     }
     bytes.push(stop_symbol);
@@ -49,13 +62,13 @@ function transmit() {
 
     transmitBytes(bytes);
 }
-function transmitBytes(bytes) {
+function transmitBytes_basic(bytes) {
     const bits = toBitArray(bytes);
     // Add bit to reset background
     bits.push("1")
     console.log(bits);
 
-    for(let i = 0; i < bits.length; i++) {
+    for (let i = 0; i < bits.length; i++) {
         const bit = bits[i];
 
         setTimeout(function () {
@@ -66,30 +79,90 @@ function transmitBytes(bytes) {
             var vis = (bit == '1' ? "white" : "black");
             transmitter.style.background = vis;
 
-            // Tick clock
-            if(clock_enabled) {
-                var clock_color = "white";
-                if (clock.style.backgroundColor.localeCompare("white") == 0) {
-                    clock_color = "black";
-                }
-                clock.style.backgroundColor = clock_color;
+            console.log(`${prev} (diff=${diff}, delay=${delay}) Color set to ${vis}`)
+        }, span * i)
+    }
+}
+function transmitBytes_clock(bytes) {
+    const bits = toBitArray(bytes);
+    // Add bit to reset background
+    bits.push("1")
+    console.log(bits);
+
+    for (let i = 0; i < bits.length; i++) {
+        const bit = bits[i];
+
+        setTimeout(function () {
+            var diff = (prev == 0 ? span : Date.now() - prev);
+            prev = Date.now()
+            delay = span + (span - diff)
+
+            var vis = (bit == '1' ? "white" : "black");
+            transmitter.style.background = vis;
+
+            var clock_color = "white";
+            if (clock.style.backgroundColor.localeCompare("white") == 0) {
+                clock_color = "black";
             }
+            clock.style.backgroundColor = clock_color;
+
 
             console.log(`${prev} (diff=${diff}, delay=${delay}) Color set to ${vis}`)
         }, span * i)
     }
 }
+function transmitBytes_brightness(bytes) {
+    const bits = toBitArray(bytes);
+    // Add bit to reset background
+    bits.push("1")
+    bits.push("1")
+    console.log(bits.length);
+
+    for (let i = 0; i < bits.length; i += 2) {
+        const bit = bits[i] + bits[i + 1];
+
+
+        setTimeout(function () {
+            var digit = parseInt(bit, 2);
+            digit *= 85;
+
+            console.log(digit)
+            var diff = (prev == 0 ? span : Date.now() - prev);
+            prev = Date.now()
+            delay = span + (span - diff)
+
+            var vis = "rgb(" + digit + ", " + digit + ", " + digit + ")";
+            transmitter.style.background = vis;
+
+
+            console.log(`${prev} (diff=${diff}, delay=${delay}) Color set to ${vis}`)
+        }, span * i)
+    }
+}
+
+function transmitBytes(bytes) {
+    const mode = mode_select.value;
+    if (mode.localeCompare("basic") == 0) {
+        transmitBytes_basic(bytes);
+    }
+    else if (mode.localeCompare("clock") == 0) {
+        transmitBytes_clock(bytes);
+    }
+    else if (mode.localeCompare("brightness") == 0) {
+        transmitBytes_brightness(bytes);
+    }
+}
 function toBitArray(bytes) {
     let result = [];
-    for(let i = 0; i < bytes.length; i++) {
+    for (let i = 0; i < bytes.length; i++) {
         const str = bytes[i];
-        for(let j = 0; j < str.length; j++) {
+        for (let j = 0; j < str.length; j++) {
             result.push(str[j]);
         }
     }
     return result;
 }
-function toBinary(val, pad_length=8) {
+function toBinary(val, pad_length = 8) {
     return (val).toString(2).padStart(pad_length, '0');
 }
 
@@ -106,11 +179,11 @@ function toUTF8Array(str) {
             utf8.push(charcode);
         } else if (charcode < 0x800) {
             utf8.push(0xc0 | (charcode >> 6),
-                      0x80 | (charcode & 0x3f));
+                0x80 | (charcode & 0x3f));
         } else if (charcode < 0xd800 || charcode >= 0xe000) {
             utf8.push(0xe0 | (charcode >> 12),
-                      0x80 | ((charcode >> 6) & 0x3f),
-                      0x80 | (charcode & 0x3f));
+                0x80 | ((charcode >> 6) & 0x3f),
+                0x80 | (charcode & 0x3f));
         } else {
             // surrogate pair
             i++;
@@ -118,11 +191,11 @@ function toUTF8Array(str) {
             // subtracting 0x10000 and splitting the
             // 20 bits of 0x0-0xFFFFF into two halves
             charcode = 0x10000 + (((charcode & 0x3ff) << 10) |
-                                    (str.charCodeAt(i) & 0x3ff));
+                (str.charCodeAt(i) & 0x3ff));
             utf8.push(0xf0 | (charcode >> 18),
-                      0x80 | ((charcode >> 12) & 0x3f),
-                      0x80 | ((charcode >> 6) & 0x3f),
-                      0x80 | (charcode & 0x3f));
+                0x80 | ((charcode >> 12) & 0x3f),
+                0x80 | ((charcode >> 6) & 0x3f),
+                0x80 | (charcode & 0x3f));
         }
     }
 
