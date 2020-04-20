e8cab49c6d5d03c3a46f7b72964112e7cec52c5c
Bram Kelchtermans <bramkel@gmail.com>
2020-04-20 14:11:00 +0200

    Added Hamming Option

--- Lab4/Opdracht_1/Transmitter/transmit.js
+++ Lab4/Opdracht_1/Transmitter/transmit.js
@@ -191,11 +191,16 @@ function transmitBytes(bytes) {
 }
 function toBitArray(bytes) {
     let result = [];
-    for (let i = 0; i < bytes.length; i++) {
-        const str = bytes[i];
-        for (let j = 0; j < str.length; j++) {
-            result.push(str[j]);
-        }
+    const hamming = document.getElementById("hamming_check").checked;
+    let str = "";
+    for(let i = 0; i < bytes.length; i++) {
+        str += bytes[i];
+    }
+    if(hamming) {
+        str = hammingEncode(str);
+    }
+    for(let i = 0; i < str.length; i++) {
+        result.push(str[i])
     }
     return result;
 }
@@ -237,4 +242,55 @@ function toUTF8Array(str) {
     }
 
     return utf8;
+}
+function hammingEncode(input) {
+	if (typeof input !== 'string' || input.match(/[^10]/)) {
+		return console.error('hamming-code error: input should be binary string, for example "101010"');
+	}
+
+	var output = input;
+	var controlBitsIndexes = [];
+	var controlBits = [];
+	var l = input.length;
+	var i = 1;
+	var key, j, arr, temp, check;
+
+	while (l / i >= 1) {
+		controlBitsIndexes.push(i);
+		i *= 2;
+	}
+
+	for (j = 0; j < controlBitsIndexes.length; j++) {
+		key = controlBitsIndexes[j];
+		arr = output.slice(key - 1).split('');
+		temp = chunk(arr, key);
+		check = (temp.reduce(function (prev, next, index) {
+			if (!(index % 2)) {
+				prev = prev.concat(next);
+			}
+			return prev;
+		}, []).reduce(function (prev, next) { return +prev + +next }, 0) % 2) ? 1 : 0;
+		output = output.slice(0, key - 1) + check + output.slice(key - 1);
+		if (j + 1 === controlBitsIndexes.length && output.length / (key * 2) >= 1) {
+			controlBitsIndexes.push(key * 2);
+		}
+	}
+
+	return output;
+}
+
+/**
+ * chunk - split array into chunks
+ * @param {Array} arr - array
+ * @param {Number} size - chunk size
+ * @returns {Array} - chunked array
+ */
+function chunk(arr, size) {
+	var chunks = [],
+	i = 0,
+	n = arr.length;
+	while (i < n) {
+		chunks.push(arr.slice(i, i += size));
+	}
+	return chunks;
 }
